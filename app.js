85b869559b28615cedaa1158b7a914c7f1e1ae18
Latorreander <latorreander@gmail.com>
2023-08-06 13:36:18 -0300

    'estilo inserido'

--- app.js
+++ app.js
@@ -4,45 +4,60 @@ const btnCalc = document.querySelector(".btn");
 const divResult = document.querySelector(".result");
 
 btnCalc.addEventListener("click", () => {
+    
+
+
     const weigthData = inputWeigth.value;
     const heightData = inputHeight.value;
 
     const calcImc = () => {
+
+     
         const result = weigthData / heightData ** 2;
 
         return Number(result.toFixed(1));
     };
 
-    const imcResult = calcImc();
+    let imcResult = calcImc();
 
     const message = () => {
+
         if (imcResult <= 18.5) {
             return "abaixo do peso";
         }
 
         if (imcResult >= 18.6 && imcResult <= 24.9) {
-            return "peso ideal (parabéns)";
+            return "Peso ideal Parabéns!";
         }
 
         if (imcResult >= 25.0 && imcResult <= 29.9) {
-            return "levemente acima do peso";
+            return "Levemente acima do Peso!";
         }
         if (imcResult >= 30.0 && imcResult <= 34.9) {
-            return "obesidade grau I";
+            return "Obesidade Grau I!";
         }
         if (imcResult >= 35.0 && imcResult <= 39.9) {
-            return "obesidade grau II (severa)";
+            return "Obesidade Grau II (severa)";
         }
 
         if (imcResult >= 40) {
-            return "obesidade III (mórbida)";
+            return "Obesidade III (mórbida)";
         }
     };
 
     const infoMessage = message() 
 
     const showResult = () => {
-        divResult.innerHTML = `Seu imc é ${imcResult} ${infoMessage}`;
+  
+
+        divResult.
+        innerHTML= `<p>Seu IMC é 
+                        <span style= "color: #B22222;
+                        font-size: 20px;">
+                            ${imcResult}
+                        </span>
+                    </p>
+                    <P>${infoMessage}</P>`
     };
 
     showResult();
